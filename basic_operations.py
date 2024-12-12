257971601905b4b041e54409058b0fb81157f149
dagim tariku <dagitariku095@gmail.com>
2024-12-12 13:31:01 +0300

    python-project

---
+++ basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+number1 = int(input("inter number1"))
+number2 =int(input("inter number2"))
+
+result = number1 + number2
+
+print(result)
+
+result2 = number1 - number2
+
+print(result2)
+
+result3 = number1 * number2
+print(result3)
