ad81bee8d320fdd7856c9102be225fd7a4d8122f
dagim tariku <dagitariku095@gmail.com>
2024-12-28 21:37:59 +0300

    weather advice commited

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+ weather = input("What's the weather like today? (sunny/rainy/cold)")
+rainy = "rainy"
+sunny = "sunny"
+coldy = "cold"
+  
+if weather == rainy:
+    print("Don't forget your umbrella and a raincoat")
+elif weather ==sunny:
+    print("Wear a t-shirt and sunglasses.")
+else:
+    print("Make sure to wear a warm coat and a scarf")
