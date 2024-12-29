933dad7e4021f08e46f284c587139dbb322d499d
dagim tariku <dagitariku095@gmail.com>
2024-12-29 11:01:20 +0300

    commited second time

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,5 +1,5 @@
-num1 = int(input("Enter the first number:"))
-num2 = int(input("Enter the first number:"))
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
 operation = input("choose the operation(+, -, *, /):")
 match operation :
        case '+':
@@ -13,8 +13,13 @@ match operation :
           result = (num1 * num2)
           print(f"the result is {result}")
        case '/':
-          result = (num1 / num2)
-          print(f"the result is {result}")
+          if num2 == 0:
+              print("cannot devided by zero")
+          else:
+              result = (num1 / num2)
+              print(f"the result is {result}")
+    
+            
        case _:
           print("invalid input")
           
